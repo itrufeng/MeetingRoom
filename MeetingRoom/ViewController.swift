956290440159efd96e23c052704d73c86a8137d0
zhaojing19890531@163.com <zhaojing19890531@163.com>
2015-05-27 22:29:06 +0800

    init

---
+++ MeetingRoom/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MeetingRoom
+//
+//  Created by betta on 5/27/15.
+//  Copyright (c) 2015 Zhao Jing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
